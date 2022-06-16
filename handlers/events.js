b020e3d033c2aea2a0bcd1103bca2cf6c901b834
Jack Doherty <jdoher8@uic.edu>
2022-06-15 23:50:38 -0500

    setup for future commands should work now along with the 'ping' command

--- handlers/events.js
+++ handlers/events.js
@@ -1,24 +1,23 @@
-const { ClientUser } = require("discord.js")
 const { getFiles } = require("../util/functions")
 
+
 module.exports = (bot, reload) => {
-    const { client } = bot
+    const {client} = bot
 
-    let event = getFiles("./events/", ".js")
+    let events = getFiles("./events/", ".js")
 
-    if (event.length === 0) {
+    if (events.legnth === 0){
         console.log("No events to load")
     }
 
-    event.forEach((f, i) => {
-        if (reload)
-            delete require.cache[require.resolve(`../event/${f}`)]
-
+    events.forEach((f, i) => {
+        if (reload) 
+            delete require.cache[require.resolve(`../events/${f}`)]
         const event = require(`../events/${f}`)
         client.events.set(event.name, event)
 
         if (!reload)
-            console.log(`${i+1}. ${f} loaded`)
+            console.log(`${i + 1}. ${f} loaded`)
     })
 
     if (!reload)
@@ -26,12 +25,12 @@ module.exports = (bot, reload) => {
 }
 
 function triggerEventHandler(bot, event, ...args){
-    const { client } = bot
+    const {client} = bot 
 
     try {
         if (client.events.has(event))
             client.events.get(event).run(bot, ...args)
-        else
+        else 
             throw new Error(`Event ${event} does not exist`)
     }
     catch(err){
@@ -39,6 +38,14 @@ function triggerEventHandler(bot, event, ...args){
     }
 }
 
-function initEvents(bot){
+function initEvents(bot) {
+    const {client} = bot 
 
-}
+    client.on("ready", () => {
+        triggerEventHandler(bot, "ready")
+    })
+
+    client.on("messageCreate", (message) => {
+        triggerEventHandler(bot, "messageCreate", message)
+    })
+} 
